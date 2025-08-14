c6b847bc8a9267ec4d61396efc0360bdde346c8c
Siddharth Dhaigude <sd8capricon@gmail.com>
2025-08-15 03:44:20 +0530

    utility to extract jurisdiction

---
+++ utils.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+
+from prompts import JURISDICTION_EXTRACTION
+from schema import Jurisdiction, Jurisdictions
+
+load_dotenv()
+
+MODEL = "gemma-3-4b-it"
+llm = ChatGoogleGenerativeAI(model=MODEL, temperature=0.25)
+
+
+def extract_jurisdiction(text: str) -> list[Jurisdiction]:
+    parser = PydanticOutputParser(pydantic_object=Jurisdictions)
+    template = PromptTemplate.from_template(JURISDICTION_EXTRACTION)
+    chain = template | llm | parser
+
+    result: Jurisdictions = chain.invoke(
+        {"text": text, "format_instructions": parser.get_format_instructions()}
+    )
+
+    if result.jurisdictions == None:
+        return []
+
+    return [juridiction for juridiction in result.jurisdictions]
